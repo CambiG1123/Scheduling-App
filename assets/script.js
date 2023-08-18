2c5c4e8dabc1f9fcf12934e3c64fdfba2fd29ff1
CambiG1123 <campbellgilliland@gmail.com>
2023-08-17 23:22:33 -0400

    used moment.js to display the current date

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
 const timeBlocks = [`#hour-1`,`#hour-2`,`#hour-3`,`#hour-4`,`#hour-5`,`#hour-9`,`#hour-10`,`#hour-11`,`#hour-12`]
+
+
+// this code displays the current date in the header 
+var dayStamp = moment().format('dddd' + ', ' + 'MMMM Do');
+  $('#currentDay').text(dayStamp);
